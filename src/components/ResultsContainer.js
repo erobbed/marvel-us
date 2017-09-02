35aae835b817ec9e5bcd6b4414d2969bdb922b21
Caitlin Keck <caitlin.keck@gmail.com>
2017-09-02 19:33:32 -0400

    added in semantic ui

--- src/components/ResultsContainer.js
+++ src/components/ResultsContainer.js
@@ -5,11 +5,11 @@ export default class ResultsContainer extends Component {
   determineResults = () => {
     switch (this.props.filter) {
       case 'comics':
-        return <p>{this.props.result !== '' ? this.props.result.results.map(result => result.title) : ""}</p>;
+        return <div className="ui grid container">{this.props.result !== '' ? this.props.result.results.map(result => <div className="ui eight wide column">{result.title}</div>) : ""}</div>;
       case 'characters':
-        return <p>{this.props.result !== '' ? this.props.result.results.map(result => result.name) : ""}</p>;
+        return <div className="ui grid container">{this.props.result !== '' ? this.props.result.results.map(result => <div className="ui eight wide column">{result.name}</div>) : ""}</div>;
       case 'creators':
-        return <p>{this.props.result !== '' ? this.props.result.results.map(result => result.firstName) : ""}</p>;
+        return <div className="ui grid container">{this.props.result !== '' ? this.props.result.results.map(result => <div className="ui eight wide column">{result.firstName}</div>) : ""}</div>;
       default:
         return <p></p>
     }
