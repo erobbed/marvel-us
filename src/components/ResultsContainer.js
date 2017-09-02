4dc6f0dfbeceea100480b60ca407e92451950ece
Caitlin Keck <caitlin.keck@gmail.com>
2017-09-02 18:33:11 -0400

    just made it so all results show up (in a very messy way)

--- src/components/ResultsContainer.js
+++ src/components/ResultsContainer.js
@@ -5,11 +5,11 @@ export default class ResultsContainer extends Component {
   determineResults = () => {
     switch (this.props.filter) {
       case 'comics':
-        return <p>{this.props.result !== '' ? this.props.result.results[0].title : ""}</p>;
+        return <p>{this.props.result !== '' ? this.props.result.results.map(result => result.title) : ""}</p>;
       case 'characters':
-        return <p>{this.props.result !== '' ? this.props.result.results[0].name : ""}</p>;
+        return <p>{this.props.result !== '' ? this.props.result.results.map(result => result.name) : ""}</p>;
       case 'creators':
-        return <p>{this.props.result !== '' ? this.props.result.results[1].firstName : ""}</p>;
+        return <p>{this.props.result !== '' ? this.props.result.results.map(result => result.firstName) : ""}</p>;
       default:
         return <p></p>
     }
