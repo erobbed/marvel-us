89096f78078e6fe6fe51d3094d1fd0c81a19ddf2
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-09-04 11:58:05 -0400

    got images to render

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
+
+
 const Character = ({character}) => {
   return (
     <div className="header">
       {console.log(character)}
       <h4>{character.name}</h4>
+      <div className="image">
+        <img src={`${character.thumbnail.path}.jpg`} height="250" width="200" alt="default"/>
+      </div>
     </div>
   )
 }
